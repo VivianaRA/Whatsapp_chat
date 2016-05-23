03bebda9a525243e3fdd3fb28bb548a2a68cfad1
Viviana Ximena Rodriguez Almanza <vrodrigueza01@gmail.com>
2016-05-23 09:37:18 -0500

    subaaaan

---
+++ index.js
@@ -0,0 +1,16 @@
+var express= require('express');
+var app= express();
+var http= require('http').Server(app);
+var io=require('socket.io')(http);
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on ('connection', function(socket){
+	console.log('Una nueva');
+});
+
+http.listen(8080, function () {
+	console.log('Express run on port 8080');
+});
